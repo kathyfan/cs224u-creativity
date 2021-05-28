be05711e268e9f45e184eab2504139390664c371
Xubo-Cao <61214545+Xubo-Cao@users.noreply.github.com>
2021-05-28 04:29:27 -0700

    Added functions for hand-built features
    I changed models.py, training.py, and Distilbert_linear_regression.ipynb such that we can add hand-picked features to the pipeline now. I also changed how the correlations were calculated during training and evaluating. It used to be calculated within each batch, but is not calculated only once within each epoch. I'm not sure how important this is, just that calculating correlation for a very small batch size seems strange to me.

--- training.py
+++ training.py
@@ -30,62 +30,123 @@ def get_iterator(test_data, batch_size, device):
         sort_within_batch = False,
     )
 
-def train(model, iterator, optimizer, criterion):
-    epoch_loss = 0
-    epoch_corr = 0
+# def train(model, iterator, optimizer, criterion):
+#     epoch_loss = 0
+#     epoch_corr = 0
   
+#     model.train() # Put model in training mode.
+
+#     for batch in iterator:
+#         optimizer.zero_grad()
+#         predictions = model(batch.text).squeeze(1)
+#         loss = criterion(predictions, batch.label)
+#         # need to use detach() since `predictions` requires gradient
+#         # alternative: scipy.stats.pearsonr? (might be more memory efficient,
+#         # but not sure which one is more efficient to compute)
+#         corr = np.corrcoef(batch.label.cpu().data.numpy(), predictions.detach().cpu().data.numpy())
+#         loss.backward()
+#         optimizer.step()
+
+#         epoch_loss += loss.item()
+#         # corr is a (2,2) matrix, so we just get the top right element.
+#         # If the correlation is a nan value, replace with 0, which means
+#         # no correlation.
+#         corr_value = corr[0][1].item()
+#         if np.isnan(corr[0][1]):
+#             corr_value = 0
+
+#         epoch_corr += corr_value
+
+#     return epoch_loss / len(iterator), epoch_corr / len(iterator)
+
+# # Evaluate the model on a validation or test set.
+# # Use debug=True to print more detailed info.
+# def evaluate(model, iterator, criterion, debug=False):
+#     epoch_loss = 0
+#     epoch_corr = 0
+
+#     model.eval()
+
+#     # i = 0
+#     with torch.no_grad():
+#         for batch in iterator:
+#             # print(i)
+#             # i += 1
+#             predictions = model(batch.text).squeeze(1)
+#             if debug:
+#                 print('predictions: {}'.format(predictions)) 
+#                 print('true labels: {}'.format(batch.label))
+#             loss = criterion(predictions, batch.label)
+#             corr = np.corrcoef(batch.label.cpu().data.numpy(), predictions.cpu().data.numpy())
+#             epoch_loss += loss.item()
+
+#             # If the correlation is a nan value, replace with 0, which means
+#             # no correlation.
+#             corr_value = corr[0][1].item()
+#             if np.isnan(corr[0][1]):
+#                 corr_value = 0
+
+#             epoch_corr += corr_value
+
+#     return epoch_loss / len(iterator), epoch_corr / len(iterator)
+
+
+def train(model, iterator, optimizer, criterion, added_features = None):
+    epoch_loss = 0
+    epoch_pred = []
+    epoch_true = []
+    
     model.train() # Put model in training mode.
 
     for batch in iterator:
         optimizer.zero_grad()
-        predictions = model(batch.text).squeeze(1)
+        predictions = model(batch.text,added_features).squeeze(1)
         loss = criterion(predictions, batch.label)
         # need to use detach() since `predictions` requires gradient
         # alternative: scipy.stats.pearsonr? (might be more memory efficient,
         # but not sure which one is more efficient to compute)
-        corr = np.corrcoef(batch.label.cpu().data.numpy(), predictions.detach().cpu().data.numpy())
         loss.backward()
         optimizer.step()
 
         epoch_loss += loss.item()
-        # corr is a (2,2) matrix, so we just get the top right element.
-        # If the correlation is a nan value, replace with 0, which means
-        # no correlation.
-        corr_value = corr[0][1].item()
-        if np.isnan(corr[0][1]):
+        epoch_pred.extend(predictions.tolist())
+        epoch_true.extend(batch.label.tolist())
+
+    corr = np.corrcoef(epoch_pred, epoch_true)
+    corr_value = corr[0][1].item()
+    if np.isnan(corr[0][1]):
             corr_value = 0
 
-        epoch_corr += corr_value
-
-    return epoch_loss / len(iterator), epoch_corr / len(iterator)
+    return epoch_loss / len(iterator), corr_value
 
 # Evaluate the model on a validation or test set.
 # Use debug=True to print more detailed info.
-def evaluate(model, iterator, criterion, debug=False):
+def evaluate(model, iterator, criterion, debug=False, added_features = None):
     epoch_loss = 0
-    epoch_corr = 0
 
     model.eval()
+    epoch_pred = []
+    epoch_true = []
 
     # i = 0
     with torch.no_grad():
         for batch in iterator:
             # print(i)
             # i += 1
-            predictions = model(batch.text).squeeze(1)
+            predictions = model(batch.text,added_features).squeeze(1)
             if debug:
                 print('predictions: {}'.format(predictions)) 
                 print('true labels: {}'.format(batch.label))
             loss = criterion(predictions, batch.label)
-            corr = np.corrcoef(batch.label.cpu().data.numpy(), predictions.cpu().data.numpy())
             epoch_loss += loss.item()
+            epoch_pred.extend(predictions)
+            epoch_true.extend(batch.label)
 
             # If the correlation is a nan value, replace with 0, which means
             # no correlation.
-            corr_value = corr[0][1].item()
-            if np.isnan(corr[0][1]):
-                corr_value = 0
+    corr = np.corrcoef(epoch_pred, epoch_true)
+    corr_value = corr[0][1].item()
+    if np.isnan(corr[0][1]):
+            corr_value = 0
 
-            epoch_corr += corr_value
-
-    return epoch_loss / len(iterator), epoch_corr / len(iterator)
+    return epoch_loss / len(iterator), corr_value
